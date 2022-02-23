2308e7fe520b542a0b4280dd04658e5ba41faa88
Black Ghost <mcramiel@gmail.com>
2022-02-23 15:53:03 +0200

    add Ocelot

--- src/Services/Question/Question.API/Application/Contracts/Dtos/QuestionAnswerDtos/QuestionAnswerUpdateDto.cs
+++ src/Services/Question/Question.API/Application/Contracts/Dtos/QuestionAnswerDtos/QuestionAnswerUpdateDto.cs
@@ -5,7 +5,6 @@ namespace Question.API.Application.Contracts.Dtos.QuestionAnswerDtos
 {
     public class QuestionAnswerUpdateDto
     {
-
         [Required(ErrorMessage = "Answer context is required")]
         [StringLength(400, MinimumLength = 3, ErrorMessage = "Answer context can't be longer than 400 characters and can't be less 3 characters")]
         public string Context { get; set; }
