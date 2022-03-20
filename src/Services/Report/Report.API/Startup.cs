80de0fb9063c46d6b888af103783acff2a466c6e
Alex <steelalex.gk@gmail.com>
2022-03-20 21:13:02 +0200

    Added email service

--- src/Services/Report/Report.API/Startup.cs
+++ src/Services/Report/Report.API/Startup.cs
@@ -2,8 +2,8 @@ using System;
 using System.Reflection;
 
 using MediatR;
-using GrpcQuestion;
 using GrpcExam;
+using GrpcQuestion;
 using FluentValidation;
 
 using Microsoft.OpenApi.Models;
@@ -18,11 +18,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Report.API.Grpc;
 using Report.API.Middleware;
 using Report.Infrastructure;
+using Report.API.Application.Models;
 using Report.API.Application.Behaviours;
+using Report.API.Application.Services.Mail;
 using Report.API.Application.Features.Queries;
+using Report.Infrastructure.Persistance.Idempotency;
 using Report.Domain.AggregatesModel.ReviewAggregate;
 using Report.Infrastructure.Persistance.Repositories;
-using Report.Infrastructure.Persistance.Idempotency;
+using Report.API.Application.Contracts.Infrastructure;
 
 
 namespace Report.API
@@ -43,25 +46,31 @@ namespace Report.API
             services.AddDbContext<ReportDbContext>(opt =>
                 opt.UseSqlServer(Configuration.GetConnectionString("ReportsConnection")));
 
+
             // Review queries configuration
             services.AddScoped<IReviewQueries, ReviewQueries>(provider => new ReviewQueries
             (Configuration.GetConnectionString("ReportsConnection")));
 
+
             // Review repository configuration
             services.AddScoped<IReviewRepository, ReviewRepository>();
 
-            // Request manager configuration
+
+            // Request manager configuration (Idempotency)
             services.AddScoped<IRequestManager, RequestManager>();
+
 
             // gRPC configuration (Question Service)
             services.AddGrpcClient<QuestionGrpc.QuestionGrpcClient>
                         (o => o.Address = new Uri(Configuration["GrpcQuestionSettings:QuestionUrl"]));
             services.AddScoped<QuestionGrpcService>();
 
+
             // gRPC configuration (Exam Service)
             services.AddGrpcClient<ExamGrpc.ExamGrpcClient>
                         (o => o.Address = new Uri(Configuration["GrpcExamSettings:ExamUrl"]));
             services.AddScoped<ExamGrpcService>();
+
 
             // CQRS configuration
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
@@ -69,8 +78,15 @@ namespace Report.API
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
 
+
+            // Email configuration
+            services.Configure<EmailSettings>(c => Configuration.GetSection("EmailSettings").Bind(c));
+            services.AddTransient<IEmailService, EmailService>();
+
+
             // Controller configuration
             services.AddControllers();
+
 
             // Swagger configuration
             services.AddSwaggerGen(c =>
